1cc053511a6e45abf672bc3dc156495e3e466640
Gasal246 <gasalgasal246@gmail.com>
2024-11-01 11:16:26 +0400

    site description changed

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -9,7 +9,7 @@ import { getUserAuth } from '@/query/server/userFunctions';
 
 export const metadata: Metadata = {
   title: "Taskmanager | Admin",
-  description: "a site by Muhammed Gasal",
+  description: "A site by Wideline IT solutions, Designed to simplify task and staff management in a business environment. Built with ease of use in mind, it allows teams to seamlessly assign, track, and complete tasks while keeping all staff data organized in one central hub. Each employee has a unique profile showcasing their roles, task history, and performance metrics. Managers can monitor task progress, set priorities, and identify bottlenecks at a glance.",
 };
 
 const AdminLayout = async ({ children }: {
